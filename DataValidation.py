60a041f826a6f02d75d192f1b3061c2a0400fd3a
jimohsodiq <serikidon32>
2020-04-06 02:41:35 -0800

    Initial commit

---
+++ DataValidation.py
@@ -0,0 +1,104 @@
+userdata = {
+
+        "password": "Jiiqlotus",
+        "password2": "Kuboroses"
+    }
+
+# User's first name
+firstname = input("Enter first name:")
+print("First name is: " + firstname)
+
+# User's second name
+secondname = input("Enter your second name: ")
+print("Second name is: " + secondname)
+
+# User's E-mail Address
+emailaddress = input("Enter your e-mail address:")
+print("E-mail address is: " + emailaddress)
+
+# Using conditional or if...statement to validate the user's password
+def password_validator(Jiiqlotus):
+    pass
+
+password = input("Enter your pasword: ")
+print("Your password is: " + password)
+if(password==("Jiiqlotus")):
+    print("Go on with it")
+    print("So these are your details: ")
+    print("Your first name is: " + " " + firstname)
+    print("Your second name is: " + " " + secondname)
+    print("Your E-mail Address is: " + " " + emailaddress)
+    print("Your password is: " + " " + password)
+else:
+
+    # Trying to collect a preferred new password from the user
+     print("I want to change my password")
+     print("Please enter your preferred password")
+     newpassword = input("Enter your password: ")
+     print("Your new password is: " +" " +newpassword)
+
+    #Checking the length of the new password given
+     if (len(newpassword)==7 or len(newpassword)>=7):
+         print("Password is the required length, password recorded")
+         print("So these are your details: ")
+         print("Your first name is: " + " " + firstname)
+         print("Your second name is: " + " " + secondname)
+         print("Your E-mail Address is: " + " " + emailaddress)
+         print("Your new password is: " + " " + newpassword)
+     else:
+         print("Password is below required, input again")
+
+         password = ["Jiiqlotus"]
+         password.append["kuboroses"]
+
+         # User's first name
+         firstname2 = input("Enter first name:")
+         print("First name is: " + firstname2)
+
+         # User's second name
+         secondname2 = input("Enter your second name: ")
+         print("Second name is: " + secondname2)
+
+         # User's E-mail Address
+         emailaddress2 = input("Enter your e-mail address:")
+         print("E-mail address is: " + emailaddress2)
+
+
+
+
+         def password_validator(kuboroses):
+             pass
+
+
+         password = input("Enter your pasword: ")
+         print("Your password is: " + password)
+         if (password == ("kuboroses")):
+             print("Go on with it")
+             print("So these are your details: ")
+             print("Your first name is: " + " " + firstname2)
+             print("Your second name is: " + " " + secondname2)
+             print("Your E-mail Address is: " + " " + emailaddress2)
+             print("Your password is: " + " " + password)
+         else:
+
+             # Trying to collect a preferred new password from the user
+             print("I want to change my password")
+             print("Please enter your preferred password")
+             newpassword2 = input("Enter your password: ")
+             print("Your new password is: " + " " + newpassword2)
+
+             # Checking the length of the new password given
+             if (len(newpassword2) == 7 or len(newpassword2) >= 7):
+                 print("Password is the required length, password recorded")
+                 print("So these are your details: ")
+                 print("Your first name is: " + " " + firstname)
+                 print("Your second name is: " + " " + secondname)
+                 print("Your E-mail Address is: " + " " + emailaddress)
+                 print("Your new password is: " + " " + newpassword2)
+             else:
+                 print("Password is below required, input again")
+
+
+
+
+
